84a16e165ee6c9332eb5ce2e6185c8c1b77d840d
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-24 05:40:12 +0100

    creation of signup and signin service and dependency injection

---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,11 @@
+export interface SignInDTO {
+  name: string;
+  password: string;
+}
+
+export interface SignUpDTO {
+  name: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { SignInDTO, SignUpDTO } from './auth.dto';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  //   signin(data: SignInDTO) {
+  //     return { data };
+  //   }
+  signin() {
+    return { msg: 'hello login' };
+  }
+
+  signup() {
+    return { msg: 'Hello signup' };
+  }
+
+  //   signup(data: SignUpDTO) {
+  //     return { data };
+  //   }
+}
