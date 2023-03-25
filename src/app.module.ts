3feeee5a633aba104caf997ff17084df2a57abbb
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-25 04:05:35 +0100

    prisma orm installation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { AuthService } from './auth/auth.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
