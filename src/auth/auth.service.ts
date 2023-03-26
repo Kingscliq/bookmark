e12b4e97a26caed864736d274a305c891174f446
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-26 07:30:56 +0100

    new migration for schema update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { SignInDTO, SignUpDTO } from './auth.dto';
 import * as argon from 'argon2';
@@ -6,27 +6,35 @@ import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
-  constructor(private dbService: DbService) { }
+  constructor(private dbService: DbService) {}
 
   async signup(request: SignUpDTO) {
     // Hash user password
-    const hash = await argon.hash(request.password);
 
-    // Create the user
-    const user = await this.dbService.user.create({
-      data: {
-        username: request.username,
-        hash,
-        firstName: request.firstName,
-        lastName: request.lastName,
-        email: request.email,
-      },
-    });
-    // Save User in the database
+    try {
+      const hash = await argon.hash(request.password);
 
-    delete user.hash;
-    console.log(user);
-    return { user };
+      // Create the user
+      const user = await this.dbService.user.create({
+        data: {
+          username: request.username,
+          hash,
+          firstName: request.firstName,
+          lastName: request.lastName,
+          email: request.email,
+        },
+      });
+      // Save User in the database
+
+      delete user.hash;
+      return { user };
+    } catch (error) {
+      if (error.code === 'p2002') {
+        throw new ForbiddenException('Email or Username Already Exists');
+      } else {
+        throw error;
+      }
+    }
   }
 
   signin() {
