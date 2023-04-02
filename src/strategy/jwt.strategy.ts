140bb31c6c202f180a8f1221af2f9745fb6dbfba
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-04-02 13:27:51 +0100

    fix: jwt strategy errors

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private jwtService: JwtService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      SecretOrKey: jwtConstants.secret,
+      secretOrKey: jwtConstants.secret,
     });
   }
 
