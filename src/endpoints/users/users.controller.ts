0b3cd2dcee688644efba8e26c4a62716704c27be
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-04-03 07:17:42 +0100

    adding a custom decorator to the users endpoint to vaidate request

--- src/endpoints/users/users.controller.ts
+++ src/endpoints/users/users.controller.ts
@@ -2,14 +2,16 @@ import { UserService } from './users.service';
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtGuard } from 'src/guards/jwt.guard';
+import { GetUser } from 'src/decorators';
+import { User } from '@prisma/client';
 
 @UseGuards(JwtGuard)
 @Controller('api/v1')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get('users')
-  fetchAllUsers(@Req() req: Request) {
-    return this.userService.fetchUsers(req.user);
+  fetchAllUsers(@GetUser() user: Partial<User>) {
+    return this.userService.fetchUsers(user);
   }
 
   @Get('user')
