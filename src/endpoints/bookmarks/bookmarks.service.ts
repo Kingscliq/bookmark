e839fb0903623a67c99ded0845b38c8a1a350667
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-04-08 08:08:12 +0100

    add: user checking for deleting a bookmark

--- src/endpoints/bookmarks/bookmarks.service.ts
+++ src/endpoints/bookmarks/bookmarks.service.ts
@@ -78,6 +78,16 @@ export class BookMarkService {
   }
 
   async deleteBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.dbService.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark || bookmark.userId !== userId) {
+      throw new ForbiddenException('Access to Resource Denied');
+    }
+
     const deletedBookmark = await this.dbService.bookmark.delete({
       where: {
         id: bookmarkId,
